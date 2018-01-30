9fc22c36e39a17b7b67bf8aa0e264bb2a1d03e0e
Ryan Sabo <sabora920@gmail.com>
2018-01-30 12:45:20 -0500

    added begining of BST class

---
+++ bst.js
@@ -0,0 +1,21 @@
+'use strict'
+
+class binarySearchTree {
+    constructor(key = null, value = null, parent = null){
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(key, value){
+        if(this.key == null){
+            this.key = key,
+            this.value = value
+        } else if(){
+            
+        }
+
+    }
+}
