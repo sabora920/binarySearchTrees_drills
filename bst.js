f4efb0eca12ea9a07c6f8fa5143b79fd1b61f653
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-30 11:45:39 -0800

    Completed the create BST and insert drill

--- bst.js
+++ bst.js
@@ -1,126 +1,21 @@
 'use strict';
 
-class BinarySearchTree {
-  constructor(key = null, value = null, parent = null){
-    this.key = key;
-    this.value = value;
-    this.parent = parent;
-    this.left = null;
-    this.right = null;
-  }
+var CircularJSON = require('circular-json');
 
-  insert(key, value){
-    if(this.key == null){
-      this.key = key;
-      this.value = value;
-    } 
-        
-    else if(key < this.key){
-      if(this.left == null) {
-        this.left = new BinarySearchTree(key, value, this);
-      }
+const BinarySearchTree = require('./bst-class');
 
-      else {
-        this.left.insert(key, value);
-      }
-    }
+const BST = new BinarySearchTree;
 
-    else {
-      if(this.right == null) {
-        this.right = new BinarySearchTree(key, value, this);
-      }
+BST.insert(3);
+BST.insert(1);
+BST.insert(4);
+BST.insert(6);
+BST.insert(9);
+BST.insert(2);
+BST.insert(5);
+BST.insert(7);
 
-      else{
-        this.right.insert(key, value);
-      }
-    }
-  }
 
-  find(key){
-    if(this.key == key){
-      return this.value;
-    } 
-    
-    else if(key < this.key && this.left) {
-      return this.left.find(key);
-    } 
-    
-    else if (key > this.key && this.right){
-      return this.right.find(key);
-    } 
-    
-    else {
-      throw new Error('Key Error');
-    }
-  }
-
-  remove(key){
-    if (key == this.key){
-      if(this.left && this.right){
-        const successor = this.right._findMin();
-        this.key = successor.key;
-        this.value = successor.value;
-        successor.remove(successor.key);
-      }
-      else if(this.left){
-        this._replaceWith(this.left);
-      }
-      else if(this.right){
-        this._replaceWith(this.right);
-      }
-      else{
-        this._replaceWith(null);
-      }
-    }
-
-    else if (key < this.key && this.left){
-      this.left.remove(key);
-    }
-
-    else if (key > this.key && this.right){
-      this.right.remove(key);
-    }
-
-    else {
-      throw new Error('Error');
-    }
-  }
-
-  _replaceWith(node) {
-    if (this.parent) {
-      if (this == this.parent.left) {
-        this.parent.left = node;
-      }
-      else if (this == this.parent.right) {
-        this.parent.right = node;
-      }
-
-      if (node) {
-        node.parent = this.parent;
-      }
-    }
-    else {
-      if (node) {
-        this.key = node.key;
-        this.value = node.value;
-        this.left = node.left;
-        this.right = node.right;
-      }
-      else {
-        this.key = null;
-        this.value = null;
-        this.left = null;
-        this.right = null;
-      }
-    }
-  }
-
-  _findMin() {
-    if (!this.left) {
-      return this;
-    }
-    return this.left._findMin();
-  }
-}
-
-module.exports = BinarySearchTree;
+BST.self = BST;
+let str = CircularJSON.stringify(BST);
+console.log(str);
