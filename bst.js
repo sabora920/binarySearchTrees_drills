e501a281200f487f862c76e376cb2f7d45e9e0f7
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-30 11:01:35 -0800

    completed the insert method on our Binary Search Tree

--- bst.js
+++ bst.js
@@ -1,21 +1,41 @@
-'use strict'
+'use strict';
 
-class binarySearchTree {
-    constructor(key = null, value = null, parent = null){
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
-        this.left = null;
-        this.right = null;
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null){
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value){
+    if(this.key == null){
+      this.key = key;
+      this.value = value;
+    } 
+        
+    else if(key < this.key){
+      if(this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+
+      else {
+        this.left.insert(key, value);
+      }
     }
 
-    insert(key, value){
-        if(this.key == null){
-            this.key = key,
-            this.value = value
-        } else if(){
-            
-        }
+    else {
+      if(this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
 
+      else{
+        this.right.insert(key, value);
+      }
     }
+  }
+
+
+
 }
