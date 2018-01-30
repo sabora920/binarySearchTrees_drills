9c3987579257a8f4e6bf432856f79900f88b21cc
Ryan Sabo <sabora920@gmail.com>
2018-01-30 15:07:12 -0500

    started outlining the findheight method

---
+++ findHeight.js
@@ -0,0 +1,36 @@
+const BinarySearchTree = require('./bst-class.js');
+
+function findHeight(searchTree){
+    
+}
+
+
+const ST = new BinarySearchTree();
+ST.insert(4, 'a')
+ST.insert(9, 'b')
+ST.insert(2, 'c')
+ST.insert(10, 'd')
+
+console.log(ST);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
